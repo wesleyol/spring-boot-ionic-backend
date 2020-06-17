ada2a98cde31ae6b34a04b0ca8982a33faec84d2
wesleyol <wesley.ol@gmail.com>
2020-06-17 00:44:35 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/wesley/cursomc/services/CategoriaService.java
+++ src/main/java/com/wesley/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.wesley.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.wesley.cursomc.domain.Categoria;
@@ -29,5 +30,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
 	}
 }
