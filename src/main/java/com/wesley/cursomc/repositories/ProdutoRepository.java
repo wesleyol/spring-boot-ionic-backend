34af3393c4a7f5204131660ab33fcb226dfbfea8
wesleyol <wesley.ol@gmail.com>
2020-06-25 16:23:21 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/wesley/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/wesley/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,20 @@
 package com.wesley.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.wesley.cursomc.domain.Categoria;
 import com.wesley.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 
 }
