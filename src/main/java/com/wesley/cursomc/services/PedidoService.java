49b581a87cb43a15da46d346fd906b0fd1d33c7b
wesleyol <wesley.ol@gmail.com>
2020-07-01 22:13:18 -0300

    Iplementando toString de Pedido

--- src/main/java/com/wesley/cursomc/services/PedidoService.java
+++ src/main/java/com/wesley/cursomc/services/PedidoService.java
@@ -11,10 +11,10 @@ import com.wesley.cursomc.domain.ItemPedido;
 import com.wesley.cursomc.domain.PagamentoComBoleto;
 import com.wesley.cursomc.domain.Pedido;
 import com.wesley.cursomc.domain.enums.EstadoPagamento;
+import com.wesley.cursomc.repositories.ClienteRepository;
 import com.wesley.cursomc.repositories.ItemPedidoRepository;
 import com.wesley.cursomc.repositories.PagamentoRepository;
 import com.wesley.cursomc.repositories.PedidoRepository;
-import com.wesley.cursomc.repositories.ProdutoRepository;
 import com.wesley.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -34,6 +34,9 @@ public class PedidoService {
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -45,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -56,10 +60,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
