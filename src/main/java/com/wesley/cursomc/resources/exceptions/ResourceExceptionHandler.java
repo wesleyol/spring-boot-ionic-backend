1fd96ae0d1e68174e7ce71bc531a364ceefe0a8a
wesleyol <wesley.ol@gmail.com>
2020-07-09 21:02:00 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/wesley/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/wesley/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.wesley.cursomc.services.exceptions.AuthorizationException;
 import com.wesley.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -41,4 +42,10 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
 }
