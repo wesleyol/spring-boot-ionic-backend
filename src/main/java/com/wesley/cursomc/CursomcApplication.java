4b1438da9b2c5ea213ea8c836c545806d875f7f1
wesleyol <wesley.ol@gmail.com>
2020-07-12 12:04:40 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/wesley/cursomc/CursomcApplication.java
+++ src/main/java/com/wesley/cursomc/CursomcApplication.java
@@ -1,44 +1,24 @@
 package com.wesley.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.wesley.cursomc.domain.Categoria;
-import com.wesley.cursomc.domain.Cidade;
-import com.wesley.cursomc.domain.Cliente;
-import com.wesley.cursomc.domain.Endereco;
-import com.wesley.cursomc.domain.Estado;
-import com.wesley.cursomc.domain.ItemPedido;
-import com.wesley.cursomc.domain.Pagamento;
-import com.wesley.cursomc.domain.PagamentoComBoleto;
-import com.wesley.cursomc.domain.PagamentoComCartao;
-import com.wesley.cursomc.domain.Pedido;
-import com.wesley.cursomc.domain.Produto;
-import com.wesley.cursomc.domain.enums.EstadoPagamento;
-import com.wesley.cursomc.domain.enums.TipoCliente;
-import com.wesley.cursomc.repositories.CategoriaRepository;
-import com.wesley.cursomc.repositories.CidadeRepository;
-import com.wesley.cursomc.repositories.ClienteRepository;
-import com.wesley.cursomc.repositories.EnderecoRepository;
-import com.wesley.cursomc.repositories.EstadoRepository;
-import com.wesley.cursomc.repositories.ItemPedidoRepository;
-import com.wesley.cursomc.repositories.PagamentoRepository;
-import com.wesley.cursomc.repositories.PedidoRepository;
-import com.wesley.cursomc.repositories.ProdutoRepository;
+import com.wesley.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-	
+
 	@Override
-	public void run(String... args) throws Exception {
-	}
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("D:\\MyApps\\Imagens\\terra.jpg");
+	}	
 }
